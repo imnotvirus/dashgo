af73a5f17efa059fc1f271ac26e8fecd8f25b002
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-07-26 20:14:07 -0300

    fix styled

--- src/components/Sidebar/NavLink/index.tsx
+++ src/components/Sidebar/NavLink/index.tsx
@@ -1,7 +1,15 @@
-import { Icon, Link as ChakraLink, Text } from "@chakra-ui/react";
-import React from "react";
-import { INavLink } from "./types";
+import {
+  Icon,
+  Link as ChakraLink,
+  LinkProps as ChakraLinkProps,
+  Text,
+} from "@chakra-ui/react";
+import React, { ElementType } from "react";
 
+interface INavLink extends ChakraLinkProps {
+  children: string;
+  icon: ElementType;
+}
 const NavLink: React.FC<INavLink> = ({ children, icon, href, ...props }) => {
   return (
     <ChakraLink display="flex" alignItems="center" {...props}>
